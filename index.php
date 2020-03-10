59509e55937d1a1df5ffcf25c7b8ea577a081cc1
SARademakers <2049137@talnet.nl>
2020-03-10 10:43:06 +0100

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -17,4 +17,57 @@ try {
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
+$series = $pdo->prepare('SELECT * FROM series');
+$series->execute();
+
+$movies = $pdo->prepare('SELECT * FROM movies');
+$movies->execute();
+
+
+function echoSeries($series)
+{
+    while ($row = $series->fetch(PDO::FETCH_OBJ))
+    {
+        echo 
+        '<tr> ' . 
+            '<td>' . $row->title . '</td>' . 
+            '<td>' . $row->rating . '</td>' . 
+        '</tr>';
+    }
+}
+
+function echoMovies($movies)
+{
+    while ($row = $movies->fetch(PDO::FETCH_OBJ))
+    {
+        echo 
+        '<tr>' . 
+            '<td>' . $row->title . '</td>' .
+            '<td>' . $row->duur . '</td>' .
+        '</tr>';
+    }
+}
+
+
 ?>
+<h2>Welkom op Netland</h2>
+<table> 
+<h3>Series</h3>
+    <tr>
+        <th>Titel</th>
+        <th>Rating</th>
+    </tr>
+<?php echoSeries($series);?>
+</table>
+
+<br>
+<br>
+
+<table> 
+<h3>Movies</h3>
+    <tr>
+        <th>Titel</th>
+        <th>Duur</th>
+    </tr>
+<?php echoMovies($movies);?>
+</table>
